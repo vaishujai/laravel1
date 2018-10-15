3fd246e9f9f79cc21105202d549a7561eaef2629
Vaishnavi <vj222@njit.edu>
2018-10-15 17:59:55 -0400

    Adding title and customizing header

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -2,7 +2,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <meta name="description" content="Learana Learning Analytics">
 <meta name="author" content="Naveena">
-<title>GSA Page</title>
+<title>Graduate student Association</title>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
       integrity="sha384-Gn5384xQqlaoWXA+058RXPxPg6fyIWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <link href="/css/app.css" rel="stylesheet">
