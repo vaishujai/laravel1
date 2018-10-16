30b6bc752eae1ead728c5eb551d6e79ba80223e9
Vaishnavi <vj222@njit.edu>
2018-10-16 18:10:39 -0400

    Editing head

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,7 +1,7 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<meta name="description" content="Learana Learning Analytics">
-<meta name="author" content="Naveena">
+<meta name="description" content="Graduate Student Association">
+<meta name="author" content="Vaishnavi">
 <title>Graduate student Association</title>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
       integrity="sha384-Gn5384xQqlaoWXA+058RXPxPg6fyIWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
